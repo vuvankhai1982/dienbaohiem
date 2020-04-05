35493cc2520853a4e7724de2dc19b550a4cd7de6
Khien Pham <phamkhien@hotmail.com>
2020-04-05 20:44:57 +0700

    Revert "Baohiem (#1)"
    This reverts commit dce0f1cb4a554ac5deeffa16083fbe034e55c519.

--- routes/web.php
+++ routes/web.php
@@ -13,14 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function(){
+Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/trangchu', 'PageController@trangchu')->name('trangchu');
-Route::get('/gioithieu', 'PageController@gioithieu')->name('gioithieu');
-Route::get('/blog', 'PageController@blog')->name('blog');
-Route::get('/lienhe', 'PageController@lienhe')->name('lienhe');
-Route::get('/goibaohiem', 'PageController@goibaohiem')->name('goibaohiem');
-Route::get('baogia', 'PageController@baogia')->name('baogia');
-Route::get('/ykien', 'PageController@ykien')->name('ykien');
