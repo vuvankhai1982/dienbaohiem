89e47526344b4a38044253858ce1eefb4ccb9b9a
QUANGKHAI <khaihoan250382@gmail.com>
2020-04-23 17:14:01 +0700

    css_lienhe1

--- resources/views/frontend/layout.blade.php
+++ resources/views/frontend/layout.blade.php
@@ -285,15 +285,33 @@
 
 </script>
 <script>
-    $(document).ready(function () {
-        $('.slider-brand').owlCarousel({
-            items: 4,
-            loop: true,
-            margin: 10,
-            autoPlay: true,
-            autoplayHoverPause: true,
-            slideSpeed: 200,
+    $(document).ready(function() {
+
+        var owl = $("#owl-demo");
+
+        owl.owlCarousel({
+            items : 6, //10 items above 1000px browser width
+            itemsDesktop : [1200,5], //5 items between 1000px and 901px
+            itemsDesktopSmall : [900,4], // betweem 900px and 601px
+            itemsTablet: [600,2], //2 items between 600 and 0
+            itemsMobile : false, // itemsMobile disabled - inherit from itemsTablet option
+            autoPlay : 3000,
         });
+
+        // Custom Navigation Events
+        $(".next").click(function(){
+            owl.trigger('owl.next');
+        })
+        $(".prev").click(function(){
+            owl.trigger('owl.prev');
+        })
+        $(".play").click(function(){
+            owl.trigger('owl.play',1000); //owl.play event accept autoPlay speed as second parameter
+        })
+        $(".stop").click(function(){
+            owl.trigger('owl.stop');
+        })
+
     });
 </script>
 </body>
