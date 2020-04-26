55efa1c9e7ac911676ae5cfa8ffc790eae602564
Pham.Khien <phamkhien@hotmail.com>
2020-04-26 15:21:34 +0700

    update

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -4348,36 +4348,5 @@
 <div id="reduce"
      style="text-align:center!important;position:fixed!important;bottom:0;width:300px;height:70px;font-size:23px;display:none;background-color:#ff3f3f;z-index=5003!important;color:#fff;line-height:1.5em!important;">
     Speed Reduced By 0.25x<br></div>
-<iframe src="./index_files/pixel.html" style="display: none;"></iframe>
-<iframe src="./index_files/webPushAnalytics.html" style="display: none;"></iframe>
-<div id="onesignal-bell-container" class="onesignal-bell-container onesignal-reset onesignal-bell-container-bottom-left"
-     aria-hidden="false">
-    <div id="onesignal-bell-launcher"
-         class="onesignal-bell-launcher onesignal-bell-launcher-md onesignal-bell-launcher-bottom-left onesignal-bell-launcher-theme-default onesignal-bell-launcher-active"
-         style="bottom: 50px;">
-        <div class="onesignal-bell-launcher-button">
-            <svg class="onesignal-bell-svg" xmlns="http://www.w3.org/2000/svg" width="99.7" height="99.7"
-                 viewBox="0 0 99.7 99.7"
-                 style="filter: drop-shadow(0 2px 4px rgba(34,36,38,0.35));; -webkit-filter: drop-shadow(0 2px 4px rgba(34,36,38,0.35));;">
-                <circle class="background" cx="49.9" cy="49.9" r="49.9" style=""></circle>
-                <path class="foreground"
-                      d="M50.1 66.2H27.7s-2-.2-2-2.1c0-1.9 1.7-2 1.7-2s6.7-3.2 6.7-5.5S33 52.7 33 43.3s6-16.6 13.2-16.6c0 0 1-2.4 3.9-2.4 2.8 0 3.8 2.4 3.8 2.4 7.2 0 13.2 7.2 13.2 16.6s-1 11-1 13.3c0 2.3 6.7 5.5 6.7 5.5s1.7.1 1.7 2c0 1.8-2.1 2.1-2.1 2.1H50.1zm-7.2 2.3h14.5s-1 6.3-7.2 6.3-7.3-6.3-7.3-6.3z"
-                      style=""></path>
-                <ellipse class="stroke" cx="49.9" cy="49.9" rx="37.4" ry="36.9" style=""></ellipse>
-            </svg>
-        </div>
-        <div class="onesignal-bell-launcher-badge"
-             style="filter: drop-shadow(0 2px 4px rgba(34,36,38,0));; -webkit-filter: drop-shadow(0 2px 4px rgba(34,36,38,0));;"></div>
-        <div class="onesignal-bell-launcher-message">
-            <div class="onesignal-bell-launcher-message-body"></div>
-        </div>
-        <div class="onesignal-bell-launcher-dialog"
-             style="filter: drop-shadow(0px 2px 2px rgba(34,36,38,.15));; -webkit-filter: drop-shadow(0px 2px 2px rgba(34,36,38,.15));;">
-            <div class="onesignal-bell-launcher-dialog-body"></div>
-        </div>
-    </div>
-</div>
-<div id="eJOY__extension_root" style="all: unset;" aria-hidden="false"></div>
 </body>
-{{--<script src="./index_files/f(1).txt"></script>--}}
 </html>
